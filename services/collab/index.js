ffac3ce2201ccf23b3eab96b2fb45c02ab508786
Harsh Suthar <bernaullis@gmail.com>
2021-08-26 17:18:49 +0530

    minor login fix

--- services/collab/index.js
+++ services/collab/index.js
@@ -58,10 +58,16 @@ const collabController = async (page, browser, client) => {
     await keyPage.goto('https://accounts.google.com/o/oauth2/auth?client_id=947318989803-6bn6qk8qdgf4n4g3pfee6491hc0brc4i.apps.googleusercontent.com&redirect_uri=urn%3aietf%3awg%3aoauth%3a2.0%3aoob&scope=email%20https%3a%2f%2fwww.googleapis.com%2fauth%2fdocs.test%20https%3a%2f%2fwww.googleapis.com%2fauth%2fdrive%20https%3a%2f%2fwww.googleapis.com%2fauth%2fdrive.photos.readonly%20https%3a%2f%2fwww.googleapis.com%2fauth%2fpeopleapi.readonly%20https%3a%2f%2fwww.googleapis.com%2fauth%2fdrive.activity.readonly%20https%3a%2f%2fwww.googleapis.com%2fauth%2fexperimentsandconfigs%20https%3a%2f%2fwww.googleapis.com%2fauth%2fphotos.native&response_type=code', { waitUntil: 'load', timeout: 0 })
     await keyPage.waitForSelector('.tgnCOd', { visible: true })
     await keyPage.click('.tgnCOd')
-    await _2stepLogin(keyPage, client)
-    const res = await keyPage.waitForXPath(`//*[@class="qhFLie"]//button`, { visible: true })
-    const signInButton = await keyPage.$x(`//*[@class="qhFLie"]//button`)
-    await signInButton.pop().click()
+    try {
+        const res = await keyPage.waitForXPath(`//*[@class="qhFLie"]//button`, { visible: true })
+        const signInButton = await keyPage.$x(`//*[@class="qhFLie"]//button`)
+        await signInButton.pop().click()
+    } catch (e) {
+        await _2stepLogin(keyPage, client)
+        const res = await keyPage.waitForXPath(`//*[@class="qhFLie"]//button`, { visible: true })
+        const signInButton = await keyPage.$x(`//*[@class="qhFLie"]//button`)
+        await signInButton.pop().click()
+    }
     await keyPage.waitForSelector('textarea', { visible: true })
     const key = await keyPage.$eval('textarea', el => el.innerHTML);
     await keyPage.close()
